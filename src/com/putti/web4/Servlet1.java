c5ec8fb237a824fc7b2118a65f5d36da09912e75
puttitang <puttitang@puttitang-macbookpro2.lan>
2018-12-29 13:43:38 +0700

    again

--- src/com/putti/web4/Servlet1.java
+++ src/com/putti/web4/Servlet1.java
@@ -27,7 +27,7 @@ public class Servlet1 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("web4-Another Served at: ").append(request.getContextPath());
+		response.getWriter().append("xxweb4-Another Served at: ").append(request.getContextPath());
 	}
 
 	/**
